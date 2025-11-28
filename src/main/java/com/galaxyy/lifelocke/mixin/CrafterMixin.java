df982487e9f2f258927f25ca41ab2c7978f0c010
Plushie <skyeplaysgames123@gmail.com>
2025-11-28 13:19:08 +0100

    Fighting types now also can't open Crafter's GUIs

---
+++ src/main/java/com/galaxyy/lifelocke/mixin/CrafterMixin.java
@@ -0,0 +1,34 @@
+package com.galaxyy.lifelocke.mixin;
+
+import com.galaxyy.lifelocke.effect.ModEffects;
+import com.galaxyy.lifelocke.gamerule.ModGameRules;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CrafterBlock;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CrafterBlock.class)
+public abstract class CrafterMixin extends Block {
+    public CrafterMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(at = @At("HEAD"), method = "onUse", cancellable = true)
+    protected void onOnUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if (player.hasStatusEffect(ModEffects.FIGHTING) && !world.isClient() &&
+        ((ServerWorld) world).getGameRules().getBoolean(ModGameRules.FIGHTING_CRAFTING_NERF)) {
+            cir.setReturnValue(ActionResult.FAIL);
+        }
+    }
+
+}
