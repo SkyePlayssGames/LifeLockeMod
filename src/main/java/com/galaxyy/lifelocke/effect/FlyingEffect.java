54e1db6168b254157df8d18cda39c879e0802b8a
Plushie <skyeplaysgames123@gmail.com>
2025-11-28 14:02:09 +0100

    Flying types now get Jump Boost V for 5 ticks when shifting

--- src/main/java/com/galaxyy/lifelocke/effect/FlyingEffect.java
+++ src/main/java/com/galaxyy/lifelocke/effect/FlyingEffect.java
@@ -9,12 +9,15 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Unit;
@@ -26,11 +29,18 @@ public class FlyingEffect extends StatusEffect {
     }
 
     @Override
+    public boolean applyUpdateEffect(ServerWorld world, LivingEntity entity, int amplifier) {
+        if (entity.isSneaking()) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 5, 4, false, false));
+        }
+        return true;
+    }
+
+    @Override
     public void onApplied(LivingEntity entity, int amplifier) {
         World world = entity.getEntityWorld();
         if (entity instanceof PlayerEntity) {
             ItemStack elytra = new ItemStack(Items.ELYTRA);
-            // ((PlayerEntity) entity).sendMessage(Text.literal("" + world.getRegistryManager().get(RegistryKeys.ENCHANTMENT).getEntry(Enchantments.BINDING_CURSE).isPresent()));
             elytra.addEnchantment(RegistryEntry.of(world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).get(Enchantments.BINDING_CURSE)), 1);
             elytra.addEnchantment(RegistryEntry.of(world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).get(Enchantments.VANISHING_CURSE)), 1);
             elytra.set(DataComponentTypes.UNBREAKABLE, Unit.INSTANCE);
