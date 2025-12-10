06b1a81e3b853bea4893445fe7dadc9ce33ea83b
Plushie <skyeplaysgames123@gmail.com>
2025-12-10 23:24:47 +0100

    Re-added Mod Menu integration based on version 17.0.0-alpha.1

--- src/main/java/com/galaxyy/lifelocke/LifeLockeModMenu.java
+++ src/main/java/com/galaxyy/lifelocke/LifeLockeModMenu.java
@@ -1,13 +1,13 @@
 package com.galaxyy.lifelocke;
 
 import com.galaxyy.lifelocke.modmenu.ModConfigScreenFactory;
-// import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-// import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 
-public class LifeLockeModMenu // implements ModMenuAPI
-{
-    // @Override
-    // public ConfigScreenFactory<?> getModConfigScreenFactory() {
-    //     return new ModConfigScreenFactory();
-    // }
+
+public class LifeLockeModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return new ModConfigScreenFactory();
+    }
 }
--- src/main/java/com/galaxyy/lifelocke/modmenu/ModConfigScreenFactory.java
+++ src/main/java/com/galaxyy/lifelocke/modmenu/ModConfigScreenFactory.java
@@ -1,12 +1,12 @@
 package com.galaxyy.lifelocke.modmenu;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
-public class ModConfigScreenFactory // implements com.terraformersmc.modmenu.api.ConfigScreenFactory
-{
-    // @Override
-    public Screen create(Screen screen) {
+public class ModConfigScreenFactory implements ConfigScreenFactory<ModConfigScreen> {
+    @Override
+    public ModConfigScreen create(Screen screen) {
         return new ModConfigScreen(Text.literal("LifeLocke Settings"), screen);
     }
 }
