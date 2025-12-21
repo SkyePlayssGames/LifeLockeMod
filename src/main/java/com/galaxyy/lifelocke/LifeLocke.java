647f27ddbdd8580b298dd6ec46d876efd5997557
Plushie <skyeplaysgames123@gmail.com>
2025-12-22 00:53:33 +0100

    Set all version numbers to 4, previous commit fixed french punctuation too

--- src/main/java/com/galaxyy/lifelocke/LifeLocke.java
+++ src/main/java/com/galaxyy/lifelocke/LifeLocke.java
@@ -33,8 +33,8 @@ import net.minecraft.util.ActionResult;
 
 public class LifeLocke implements ModInitializer {
 	public static final String MOD_ID = "lifelocke";
-	public static final int CLIENT_VERSION = 3;
-	public static final int SERVER_VERSION = 3;
+	public static final int CLIENT_VERSION = 4;
+	public static final int SERVER_VERSION = 4;
 
 	@Override
 	public void onInitialize() {
--- src/main/java/com/galaxyy/lifelocke/event/CheckServerVersionEvent.java
+++ src/main/java/com/galaxyy/lifelocke/event/CheckServerVersionEvent.java
@@ -5,7 +5,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 
 public class CheckServerVersionEvent implements ModdedPlayerJoinServerCallback {
-    private static final int MINIMUM_VERSION = 3;
+    private static final int MINIMUM_VERSION = 4;
 
     @Override
     public ActionResult check(ServerPlayerEntity player, int version) {
