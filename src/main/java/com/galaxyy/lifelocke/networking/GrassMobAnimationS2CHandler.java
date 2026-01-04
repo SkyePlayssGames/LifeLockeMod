a400bedd54b5ebf6cae8b462f3d53dfe9d98e2c5
Plushie <skyeplaysgames123@gmail.com>
2026-01-05 00:26:07 +0100

    Added some error logging to the GrassMobAnimationS2CHandler that should hopefully never be called but god knows

--- src/main/java/com/galaxyy/lifelocke/networking/GrassMobAnimationS2CHandler.java
+++ src/main/java/com/galaxyy/lifelocke/networking/GrassMobAnimationS2CHandler.java
@@ -8,15 +8,24 @@ public class GrassMobAnimationS2CHandler implements ClientPlayNetworking.PlayPay
     @Override
     public void receive(GrassMobAnimationS2CPayload grassMobAnimationS2CPayload, ClientPlayNetworking.Context context) {
         Entity entity = context.player().getEntityWorld().getEntity(grassMobAnimationS2CPayload.uuid());
-        if (!(entity instanceof GrassMobEntity grassMob)) {
-            return;
+        if (!(entity instanceof GrassMobEntity)) {
+            if (entity == null) {
+                System.err.println("Tried playing a Grass Mob Animation on a null entity");
+            } else {
+                System.err.println("Tried playing a Grass Mob Animation on entity \"" + entity.getType() +
+                        "\" and UUID " + entity.getUuid() + "\"");
+            }
         }
+
+        assert entity instanceof GrassMobEntity;
+        GrassMobEntity grassMob = ((GrassMobEntity) entity);
 
         switch (GrassMobAnimationS2CPayload.ANIMATION.fromString(grassMobAnimationS2CPayload.animation())) {
             case HIDE -> grassMob.playHidingAnimation = true;
             case UNHIDE -> grassMob.playUnhidingAnimation = true;
             case MAGIC_ATTACK -> grassMob.playMagicAttackAnimation = true;
-            case null -> {}
+            case null -> System.err.println("Tried playing invalid animation \"" + grassMobAnimationS2CPayload.animation() +
+                    "\" for GrassMobEntity with UUID \"" + grassMob.getUuidAsString() + "\"");
         }
     }
 }
