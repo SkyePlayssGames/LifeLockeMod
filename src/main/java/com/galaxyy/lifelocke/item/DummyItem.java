054ba7c312a3bb619b82b871b557fb1a2c134516
Plushie <skyeplaysgames123@gmail.com>
2026-01-06 20:58:07 +0100

    removed non-working code in DummyItem

--- src/main/java/com/galaxyy/lifelocke/item/DummyItem.java
+++ src/main/java/com/galaxyy/lifelocke/item/DummyItem.java
@@ -1,22 +1,24 @@
 package com.galaxyy.lifelocke.item;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3i;
+
+import java.util.function.UnaryOperator;
 
 public class DummyItem extends Item {
     public DummyItem(Settings settings) {
         super(settings);
-    }
-
-    @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        context.getPlayer().setCustomName(Text.literal("Hello Kitty!"));
-        return ActionResult.SUCCESS;
     }
 }
