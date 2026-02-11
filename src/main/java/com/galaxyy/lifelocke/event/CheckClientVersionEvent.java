20304e49977a8ffbc3c0d2cc6e360ddff16e6a79
Plushie <skyeplaysgames123@gmail.com>
2026-02-11 15:45:53 +0100

    .

--- src/main/java/com/galaxyy/lifelocke/event/CheckClientVersionEvent.java
+++ src/main/java/com/galaxyy/lifelocke/event/CheckClientVersionEvent.java
@@ -5,7 +5,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 
 public class CheckClientVersionEvent implements ModdedPlayerJoinClientCallback {
-    private static final int MAXIMUM_SERVER_VERSION = 17;
+    private static final int MAXIMUM_SERVER_VERSION = 20;
 
     @Override
     public InteractionResult check(Player player, int version) {
